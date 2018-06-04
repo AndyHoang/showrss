ec28eb9644740ff7789d3ae58b9a667c47b77b49
andyhoang <andy@parcelperform.com>
2018-06-04 10:13:05 +0700

    now can pipe magnet to webtorrent

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,8 @@ error_chain!{
     }
 }
 use rss::{Channel, Item};
-use rss::extension::{ExtensionMap};
 use std::convert::From;
-
+use std::process::Command;
 
 impl From<Item> for Episode{
 
@@ -33,7 +32,6 @@ impl From<Item> for Episode{
             .and_then(|vec| vec.into_iter().last().and_then(|e| e.value()));
         let link = item.link().map(String::from).map_or_else(|| Vec::new(), |link| vec![link]);
         let e: Episode = Episode{title:item.title().map(String::from ), show_name: show_name.map(String::from), magnet_links: link, ..Default::default()};
-        println!("{:?}", e);
         e
     }
 }
@@ -44,12 +42,34 @@ struct Episode{
     magnet_links: Vec<String>,
     http_links: Vec<String>
 }
-
+use std::io::{self};
 
 
 fn main() {
     let channel = Channel::from_url("http://showrss.info/user/105107.rss?magnets=true&namespaces=true&name=null&re=null").unwrap();
     let items = channel.into_items();
     let episodes :Vec<Episode> = items.into_iter().map(|x| Episode::from(x)).collect();
+    for (i, ep) in episodes.iter().enumerate(){
+        println!("{:?} {:?}", i, ep.title);
+    }
+    println!("select ep to stream!");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let selector = input.trim().parse::<u32>().unwrap();
+            let selected = episodes.get(selector as usize).unwrap();
+            let link :&str= selected.magnet_links.last().unwrap();
+            println!("piping to iina {:?}", link);
+            let the_output = Command::new("webtorrent")
+                    .args(&[link, "--iina"])
+                    .output()
+                    .ok()
+                    .expect("failed to execute process");
+            let encoded = String::from_utf8_lossy(the_output.stdout.as_slice());
+            print!("{}", encoded);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
 
 }
